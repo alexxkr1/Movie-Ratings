6bdeba3e83e07aa375e954cb28367e1cf3f97839
Alexxkr1 <alexandroskrenstrauch@gmail.com>
2023-06-23 16:23:46 +0300

    use api key secret from env

--- src/axios.ts
+++ src/axios.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const httpClient = axios.create({
   baseURL: "https://api.themoviedb.org/",
   headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMjUxOWNiZjlkYzc5YjFjZDYxYTI4M2I3YjhhOGI3ZCIsInN1YiI6IjY0OTRiM2ZjZDIzNmU2MDExZTA5ODg3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._C-U5_IOlj-OSQBtg00EShhXDwyJdgrVLFIE4IUS48M",
+    Authorization: `Bearer ${import.meta.env.moviesApiKey}`,
   },
 });
